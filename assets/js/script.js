b953940adec8e1294bbcc24908f4d811405d7c4b
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-05-12 14:50:43 +0200

    drag n drop a peu près fini

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
+    $(".card").draggable({ // Rend l'élément draggable
+      revert : 'invalid',// L'élément revient à sa place si il n'est pas droppé au bon endroit
+      snap : '#emptyBlock'// Effet de magnétisme avec l'élément sélectionné
+  });
+    $( "#emptyBlock" ).droppable({// Rend l'élément droppable
+    accept: ".card",// Permet de choisir les éléments acceptés dans le drop
+    classes: {// Feedback lorsque un élément droppable est séléctionné
+      "ui-droppable-active": "ui-state-default"
+    },
+});
 });
