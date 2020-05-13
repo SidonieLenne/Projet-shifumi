a331f3dd122a2b037a707e283ce4a8ef943cd495
HerchuelzArthur <arthurherchuelz@gmail.com>
2020-05-13 17:03:56 +0200

    fin de journée

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     //Déclaration de la variable tableau ordi
     var array = ['Pierre', 'Feuille', 'Ciseaux'];
-    //Déclaration des variables compteur de victoires/défaites/essais/userMove qui sont à 0/null au début  
+    //Déclaration des variables compteur de victoires/défaites/essais/userMove qui sont à 0/null au début
     var wins = 0;
     var loses = 0;
     var tryCount = 0;
@@ -10,15 +10,17 @@ $(document).ready(function(){
     //............................................Bouton Réinitialiser........................................................
     $('#tryAgain').click(function(){
         wins = 0;//Ne fonctionne pas
-        loses = 0;//Ne fonctionne pas 
+        loses = 0;//Ne fonctionne pas
         tryCount = 0;//Ne fonctionne pas
-        $('#randomBlock').css('background-image', 'none');  
+        $('#victory').text('Moi : ' + wins);
+        $('#defeat').text('Ordinateur : ' + loses);
+        $('#randomBlock').css('background-image', 'none');
     });
-    //...........................................Rend les réponses draggables..............................................   
+    //...........................................Rend les réponses draggables..............................................
     $(".answer").draggable({
         revert : 'valid',//Renvoie l'élément à sa place une fois droppé
         snap : '#emptyBlock',//Elles sont attirées par le bloc droppable
-        
+
     });
     //Quand le clic de la souris se relève, ça fait passer userMove de null à "Pierre", "Feuille" ou "Ciseaux" selon le bloc qui est cliqué
     $('#firstCard').mouseup(function(){
@@ -30,14 +32,14 @@ $(document).ready(function(){
     $('#thirdCard').mouseup(function(){
         userMove = 'Ciseaux';
     });
-    //............................................Rend le "déposez ici" droppable...........................................    
+    //............................................Rend le "déposez ici" droppable...........................................
     $( "#emptyBlock" ).droppable({
         accept: ".answer", //Le bloc n'accepte de recevoir que les blocs avec la classe answer
         classes: {
-            "ui-droppable-active": "ui-state-default"    
+            "ui-droppable-active": "ui-state-default"
         },
         //..............Une fois qu'on drop un élément dans "emptyBlock", cette fonction se déclenche............
-        drop:function(){ 
+        drop:function(){
             //Choisis au hasard une valeur dans le tableau déclaré plus haut
             var ordiMove = array[Math.floor(array.length * Math.random())];
             //Met les images correspondantes au choix de l'ordi sur randomBlock
@@ -52,6 +54,7 @@ $(document).ready(function(){
             //SI USER = ORDI, alerte Egalité, +1 compteur d'essais
             if (userMove == ordiMove){
                 tryCount++;
+                wins = wins + 0;
                 alert('Egalité!');
                 //SINON SI USER = Pierre et ORDI = Ciseaux, alerte Gagné, +1  compteur d'essais, +1 compteur gagné
             } else if (userMove == 'Pierre' && ordiMove == 'Ciseaux'){
@@ -63,22 +66,22 @@ $(document).ready(function(){
                 wins++;
                 tryCount++;
                 alert('Gagné!');
-                //SINON SI USER = Ciseaux et ORDI = Feuille, alerte Gagné, +1  compteur d'essais, +1 compteur gagné    
+                //SINON SI USER = Ciseaux et ORDI = Feuille, alerte Gagné, +1  compteur d'essais, +1 compteur gagné
             } else if (userMove == 'Ciseaux' && ordiMove == 'Feuille'){
                 wins++;
                 tryCount++;
                 alert('Gagné!');
-                //SINON SI ORDI = Pierre et USER = Ciseaux, alerte Perdu, +1  compteur d'essais, +1 compteur perdu    
+                //SINON SI ORDI = Pierre et USER = Ciseaux, alerte Perdu, +1  compteur d'essais, +1 compteur perdu
             } else if (ordiMove == 'Pierre' && userMove == 'Ciseaux'){
                 loses++;
                 tryCount++;
                 alert('Perdu...');
-                //SINON SI ORDI = Feuille et USER = Pierre, alerte Perdu, +1  compteur d'essais, +1 compteur perdu       
+                //SINON SI ORDI = Feuille et USER = Pierre, alerte Perdu, +1  compteur d'essais, +1 compteur perdu
             } else if (ordiMove == 'Feuille' && userMove == 'Pierre'){
                 loses++;
                 tryCount++;
                 alert('Perdu...');
-                //SINON SI ORDI = Ciseaux et USER = Feuille, alerte Perdu, +1  compteur d'essais, +1 compteur perdu       
+                //SINON SI ORDI = Ciseaux et USER = Feuille, alerte Perdu, +1  compteur d'essais, +1 compteur perdu
             } else if (ordiMove == 'Ciseaux' && userMove == 'Feuille'){
                 loses++;
                 tryCount++;
